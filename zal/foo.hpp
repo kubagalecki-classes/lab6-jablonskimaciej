6a8133791610cd4b74621bc8bdbe7a8eb5db0234
Maciej Jabłoński <73608866+jablonskimaciej@users.noreply.github.com>
2020-12-16 17:41:37 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -12,5 +12,15 @@ std::vector< char > foo(std::list< Human >& people)
 
     // Twoja implementacja tutaj
 
+    std::for_each(people.begin(), people.end(), [](Human& people) { people.birthday(); });
+    std::transform(people.begin(), people.end(), ret_v.begin(), [](Human& people) -> char {
+        if (people.isMonster() == true)
+            return 'y';
+        else
+            return 'n';
+    });
+
+    std::reverse(ret_v.begin(), ret_v.end());
+    
     return ret_v;
 }
